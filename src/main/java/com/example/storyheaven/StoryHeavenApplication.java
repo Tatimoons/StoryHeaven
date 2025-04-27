3cb32d12b7949914267285801674eb97ceb1d389
Nartai <nartay.seylkhan@gmail.com>
2025-04-27 20:31:30 +0500

    First commit 27.04

---
+++ src/main/java/com/example/storyheaven/StoryHeavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.storyheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryHeavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryHeavenApplication.class, args);
+    }
+
+}
