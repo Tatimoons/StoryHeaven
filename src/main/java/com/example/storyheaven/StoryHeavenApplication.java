9fa83cb1805890d93ba23e953de3278e20d8c3b1
Nartai <nartay.seylkhan@gmail.com>
2025-04-29 21:34:56 +0500

    Добавляем админа в StoryHeavenApplication.java

--- src/main/java/com/example/storyheaven/StoryHeavenApplication.java
+++ src/main/java/com/example/storyheaven/StoryHeavenApplication.java
@@ -1,13 +1,38 @@
 package com.example.storyheaven;
 
+import com.example.storyheaven.entity.User;
+import com.example.storyheaven.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class StoryHeavenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoryHeavenApplication.class, args);
     }
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public CommandLineRunner initAdmin() {
+        return args -> {
+            if (userRepository.findByEmail("admin@storyheaven.kz").isEmpty()) {
+                User admin = User.builder()
+                        .username("admin")
+                        .email("admin@storyheaven.kz")
+                        .password(passwordEncoder.encode("admin"))
+                        .role("ROLE_ADMIN")
+                        .build();
+                userRepository.save(admin);
+                System.out.println("Администратор создан: admin@storyheaven.kz / admin123");
+            }
+        };
+    }
 }
